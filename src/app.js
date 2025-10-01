718587ce9c4784130793c0809dc072d278b61aa1
Gautam Dhodi <gautamdhodi02@gmail.com>
2025-10-01 17:45:53 +0530

    fixed sum bugs and updated app.js

---
+++ src/app.js
@@ -0,0 +1,49 @@
+import express  from "express";
+
+import cors from "cors"
+
+import cookieParser from "cookie-parser"
+
+import userRouter from "../src/routers/user.routes.js"
+
+import reportRouter from "../src/routers/report.routes.js"
+import {rateLimit} from "express-rate-limit"
+import dotenv from 'dotenv';
+dotenv.config();
+
+
+const app = express()
+
+const limiter = rateLimit({
+	windowMs: 10 * 60 * 1000, // 10 minutes
+	limit: 50, // Limit each IP is 20
+	standardHeaders: 'draft-8', 
+	legacyHeaders: false, 
+	
+})
+
+app.use(cors({
+    origin: process.env.CORS_ORIGIN,
+    credentials:true
+})) 
+
+app.use(express.json({limit:"16kb"}));
+
+app.use(express.urlencoded({extended:true,limit:"16kb"}))
+
+app.use(express.static("public"))
+
+app.use(cookieParser())
+
+app.use(limiter)
+
+// Routes
+
+app.use("/api/v1/users", userRouter)
+app.use("/api/v1/report", reportRouter)
+app.get("/healthz", (req, res) => {
+    res.status(200).json({ status: "OK" });
+  });
+
+
+export {app}
